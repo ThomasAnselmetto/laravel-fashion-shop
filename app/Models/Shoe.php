8989e131be16efc8eafc8e722bf5cd0e545eea50
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-04-18 19:11:29 +0200

    init soft delete + userseeder

--- app/Models/Shoe.php
+++ app/Models/Shoe.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Shoe extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $fillable = ["model", "type", "number", "color", "quantity", "image"];
     public function getAbstract($max = 40)
--- database/migrations/2023_04_14_151542_create_shoes_table.php
+++ database/migrations/2023_04_14_151542_create_shoes_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string("color");
             $table->integer("quantity");
             $table->string("image");
+            $table->softDeletes();
             $table->timestamps();
         });
     }
